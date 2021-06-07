82080b72c3fc8a526eb7102481ee509e9c139b80
TANVISHARMA08 <tanvi8sh@gmail.com>
2021-06-07 13:25:29 +0530

    ADDRESS BOOK APPLICATION

---
+++ JS/site_properties.js
@@ -0,0 +1,6 @@
+let site_properties = {
+    use_local_storage: "false",
+    home_page: "../pages/home.html",
+    add_address_book_form_page: "../pages/Address-Book  form.html",
+    server_url: "http://localhost:3000/ADDRESSBOOK DB"
+};
